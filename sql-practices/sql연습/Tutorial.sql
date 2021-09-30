85945a078ee823af11a7fb26572d5280e85157f3
jin0kim326 <53967482+jin0kim326@users.noreply.github.com>
2021-09-30 16:07:00 +0900

    first commit

---
+++ sql-practices/sql연습/Tutorial.sql
@@ -0,0 +1,12 @@
+drop table pet;
+
+create table pet (
+	 name varchar(20),
+     owner varchar(20),
+     sepecies varchar(20),
+     gender char(1),
+     birth DATE,
+     death DATE
+);
+
+desc pet;
