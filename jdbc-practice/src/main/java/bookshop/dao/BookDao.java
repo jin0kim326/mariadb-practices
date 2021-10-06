47af46d39a1cb0760e6cfc9d5bfa0c1caa19ec8d
jin0kim326 <53967482+jin0kim326@users.noreply.github.com>
2021-10-06 15:18:45 +0900

    1016#5 dao pattern2

---
+++ jdbc-practice/src/main/java/bookshop/dao/BookDao.java
@@ -0,0 +1,64 @@
+package bookshop.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import bookshop.vo.BookVo;
+
+public class BookDao {
+	public boolean insert(BookVo vo) {
+		boolean result = false;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		try {
+			conn = getConnection();
+			
+			//SQL 준비
+			String sql = "INSERT INTO book values (null, ?, ?, ?)";
+			pstmt = conn.prepareStatement(sql);
+			
+			//바인딩 (binding)
+			pstmt.setString(1, vo.getTitle());
+			pstmt.setLong(2, vo.getAuthorNo());
+			pstmt.setString(3, vo.getStatus());
+			
+			//5. SQL 실행
+			int count = pstmt.executeUpdate();
+			
+			result = count == 1;
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if ( conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			
+		}
+		return result;
+	}
+	
+	private Connection getConnection() throws SQLException {
+		Connection conn = null;
+		
+		try {
+			// 1. JDBC Driver 로딩
+			Class.forName("org.mariadb.jdbc.Driver");
+			// 2. 연결하기
+			String url = "jdbc:mysql://127.0.0.1:3306/webdb?charset=utf8";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패: "+e);
+		}
+		return conn;
+	}
+
+}
